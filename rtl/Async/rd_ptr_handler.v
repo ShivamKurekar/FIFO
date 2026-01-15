15a1d279eb9a78406d0a428d6212b9ae4a1e993f
ShivamKurekar <kurekarshivam@gmail.com>
2026-01-15 14:59:25 +0530

    issue(rd_ptr_handler.v): o_b_rd_ptr bus width was missing the MSB
    Increased the width

--- rtl/Async/rd_ptr_handler.v
+++ rtl/Async/rd_ptr_handler.v
@@ -3,7 +3,7 @@
 module rd_ptr_handler #(parameter PTR_WIDTH)(
     input i_clk, i_rstn, i_en,
     input  [PTR_WIDTH : 0] i_g_wr_ptr,
-    output [(PTR_WIDTH - 1) : 0] o_b_rd_ptr, // to fifo mem
+    output reg [PTR_WIDTH : 0] o_b_rd_ptr, // to fifo mem
     output reg [PTR_WIDTH : 0] o_g_rd_ptr, // gray ptr o/p to ff_sync
     output reg o_empty
 );
@@ -12,16 +12,18 @@ module rd_ptr_handler #(parameter PTR_WIDTH)(
     wire rd_empty;
 
     always @(posedge i_clk or negedge i_rstn) begin
+
         if(!i_rstn) begin
             o_b_rd_ptr <= 0;
             o_g_rd_ptr <= 0;
-            o_empty <= 0;
+            o_empty <= 1'b1;
         end
         else begin
             o_b_rd_ptr <= nxt_b_rd_ptr;
             o_g_rd_ptr <= nxt_g_rd_ptr;
             o_empty <= rd_empty;
         end
+
     end
 
     // next binary ptr calculation
